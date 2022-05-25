9e687968b33f35a63f3bb90f6b7ea7fcf25d1cb2
Ivan Voskoboinyk <ivan@voskoboinyk.com>
2022-05-25 17:29:20 +0300

    Let TS help us avoid mistakes of accessing properties on null
    See https://github.com/prezly/uploads-js/pull/1/files#r881712935

--- src/guards/isUploadedImage.ts
+++ src/guards/isUploadedImage.ts
@@ -1,23 +1,20 @@
-import { isPlainObject } from 'is-plain-object';
 import { isUploadedFile } from './isUploadedFile';
 import type { UploadedImage } from '../types';
+import { isObject } from './isObject';
 
 function isNonEmptyString(value: unknown): boolean {
     return typeof value === 'string' && value.length > 0;
 }
 
-export function isUploadedImage(
-    file: Record<string, any> | undefined | null,
-): file is UploadedImage {
+export function isUploadedImage(file: unknown): file is UploadedImage {
     return (
-        typeof file === 'object' &&
-        isPlainObject(file) &&
-        typeof file?.original_height === 'number' &&
+        isObject(file) &&
+        isUploadedFile(file) &&
+        typeof file.original_height === 'number' &&
         typeof file.original_width === 'number' &&
         file.original_height > 0 &&
         file.original_width > 0 &&
         Array.isArray(file.effects) &&
-        file.effects.every(isNonEmptyString) &&
-        isUploadedFile(file)
+        file.effects.every(isNonEmptyString)
     );
 }
